d13c04e9edfccfbf45a4808bcf751caa777cfe3b
Semir_S <149691609+Semirss@users.noreply.github.com>
2025-07-21 04:54:59 +0300

    fg

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,9 +3,8 @@ import './AboutSection.css';
 
 const AboutSection: React.FC = () => {
   return (
-    <section className=" py-8 sm:py-12 md:py-16 lg:py-20 px-4 sm:px-6 md:px-8 overflow-hidden mx-auto" style={{justifyContent: 'center'}}>
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-6 sm:gap-8 md:gap-12 relative"style={{justifyContent: 'center'}}>
-        <div className="w-full md:w-1/2 relative z-20"style={{justifyContent: 'center'}}>
+    <section id='sec' className=" py-8 sm:py-12 md:py-16 lg:py-20 px-4 sm:px-6 md:px-8 overflow-hidden mx-auto" style={{justifyContent: 'center', width:'80%'}}>
+    
           <div className="relative bg-white/90 backdrop-blur-sm p-4 sm:p-6 md:p-8 rounded-xl shadow-xl overflow-visible flex flex-col items-center sm:items-start sm:flex-row gap-4 sm:gap-6">
             <div className="flex justify-center w-full sm:w-auto">
             </div>
@@ -15,10 +14,12 @@ const AboutSection: React.FC = () => {
                 Natanem <span className="text-pink-600">Couture</span>
               </h2>
               <p
+
                 className="text-gray-800 text-center sm:text-left text-base sm:text-lg md:text-xl leading-relaxed mb-6 max-h-48 sm:max-h-60 font-sans no-scrollbar"
                 style={{
                   overflowY: 'auto',
-                  padding: '1rem',
+                  padding: '3rem',
+                  textAlign: 'justify',
                 }}
                 onScroll={(e) => {
                   if (e.currentTarget.scrollLeft !== 0) e.currentTarget.scrollLeft = 0;
@@ -44,8 +45,7 @@ const AboutSection: React.FC = () => {
               className="absolute -bottom-4 sm:-bottom-6 md:-bottom-8 -left-4 sm:-left-6 md:-left-8 w-20 sm:w-24 md:w-28 transform -rotate-6 rounded-lg shadow-xl border-4 border-white z-0"
             />
           </div>
-        </div>
-      </div>
+
     </section>
   );
 };
