bee89e62be1953252c37c659feb3370ae4426500
Semir_S <149691609+Semirss@users.noreply.github.com>
2025-07-21 12:35:32 +0300

    h

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -147,12 +147,13 @@ const GalleryPage: React.FC = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6 text-center">{item.name}</h1>
+     
       <img
         src={item.image}
         alt={item.name}
         className="w-full max-w-2xl mx-auto h-auto rounded-lg shadow-lg"
-      />
+      /> 
+      <h1 className="text-3xl font-bold mb-6 text-center">{item.name}</h1>
       <p className="text-lg mt-4 text-center">{item.description}</p>
       <button
         className="mt-4 px-4 py-2 bg-pink-600 text-white rounded hover:bg-grey-700 mx-auto block"
